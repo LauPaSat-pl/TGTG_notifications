ea938258601cb7cbee7ed041b8cacc2ba031cf57
LauPaSat-pl <56610392+LauPaSat-pl@users.noreply.github.com>
2023-04-03 09:59:05 +0200

    Added loading secrets to an action file

--- main.py
+++ main.py
@@ -14,16 +14,20 @@ async def main() -> None:
 	"""
 	access_token: str = os.environ['ACCESS_TOKEN']
 	refresh_token: str = os.environ["REFRESH_TOKEN"]
-	user_id: str = os.environ['USER_ID']
+	tgtg_user_id: str = os.environ['TGTG_USER_ID']
 	cookie: str = os.environ['COOKIE']
 	telegram_bot_id: str = os.environ['TG_BOT_ID']
 	telegram_chat_id: str = os.environ['TG_CHAT_ID']
-	tgtg_client: TgtgClient = TgtgClient(access_token=access_token, refresh_token=refresh_token, user_id=user_id, cookie=cookie)
+	tgtg_client: TgtgClient = TgtgClient(access_token=access_token,
+	                                     refresh_token=refresh_token,
+	                                     user_id=tgtg_user_id,
+	                                     cookie=cookie)
 	bot: Bot = Bot(telegram_bot_id)
 	for item in tgtg_client.get_items():
 		items_available: int = item['items_available']
 		if items_available > 0:
-			await bot.send_message(chat_id=telegram_chat_id, text=f"There are {items_available} bags")
+			await bot.send_message(chat_id=telegram_chat_id,
+			                       text=f"There are {items_available} bags")
 
 
 if __name__ == '__main__':
