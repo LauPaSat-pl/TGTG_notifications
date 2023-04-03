ef84a8884141542da481e6741dbd971455116900
LauPaSat-pl <56610392+LauPaSat-pl@users.noreply.github.com>
2023-04-03 09:34:49 +0200

    Telegram version

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+App to check available favourite packages from Too Good To Go, and send Telegram bot message
+"""
+import asyncio
+import os
+
+from telegram import Bot
+from tgtg import TgtgClient
+
+
+async def main() -> None:
+	"""
+	Main function of the script
+	"""
+	access_token: str = os.environ['ACCESS_TOKEN']
+	refresh_token: str = os.environ["REFRESH_TOKEN"]
+	user_id: str = os.environ['USER_ID']
+	cookie: str = os.environ['COOKIE']
+	telegram_bot_id: str = os.environ['TG_BOT_ID']
+	telegram_chat_id: str = os.environ['TG_CHAT_ID']
+	tgtg_client: TgtgClient = TgtgClient(access_token=access_token, refresh_token=refresh_token, user_id=user_id, cookie=cookie)
+	bot: Bot = Bot(telegram_bot_id)
+	for item in tgtg_client.get_items():
+		items_available: int = item['items_available']
+		if items_available > 0:
+			await bot.send_message(chat_id=telegram_chat_id, text=f"There are {items_available} bags")
+
+
+if __name__ == '__main__':
+	asyncio.run(main())
